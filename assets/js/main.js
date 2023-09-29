25d44e01c9c011696ebaa522578031e6b9a2a32e
jenniewillson <>
2023-09-29 20:13:48 +0000

    FEAT:Amended countdown to finish at 0

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,5 +42,6 @@ function checkLetter(event) {
 
 var count = 60, timer = setInterval(function () {
     $("#counter").html(count--);
-    if (count == 1) clearInterval(timer);
+    if (count == -1) clearInterval(timer);
 }, 1000);
+
