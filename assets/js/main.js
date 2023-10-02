c77ed1ad81dcc4556977f339f4667f0f02a894e4
jenniewillson <>
2023-10-02 21:34:55 +0000

    FEAT:Added Bootstrap modal for detailed instructions and styled

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,14 +78,9 @@ function checkLetter(event) {
 //provides the countdown
 
 function startTime() {
-    var count = 60, timer = setInterval(function () {
+    let count = 60, timer = setInterval(function () {
     $("#counter").html(count--);
     if (count == -1) clearInterval(timer);
 }, 1000);
 };
 
-//detailed instructions pop-up
-
-$('#d-instructions').click(function () {
-    window.alert("Select the number of letters you would like in the words you are shown.\r\rWhen you click in the box, the timer will start.You have a minute to types as many words as possible.\r\rIf you get a word wrong, your turn ends.Click the selector for the number of letters to start a new game.\r\rThe more words you get correct, the more animals you gain in your zoo!If you get all your words correct, you win! \r\rKeep trying to beat your high score!");
-});
