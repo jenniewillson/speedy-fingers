53ea1ed56744d148f2d4143d6cbce88ab1cc0540
jenniewillson <>
2023-10-11 23:03:09 +0000

    REFAC:Consistent quotes

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,7 +37,7 @@ function gameOnOff() {
 
 //contacts the API and returns error if needed
 
-const baseURL = "https://random-word-api.vercel.app/api?words=200";
+const baseURL = 'https://random-word-api.vercel.app/api?words=200';
 
 function getData(noLetters, cb) {
     var xhr = new XMLHttpRequest();
@@ -47,13 +47,13 @@ function getData(noLetters, cb) {
                 //Successfully called API
                 cb(JSON.parse(this.responseText));
             } else {
-                document.getElementById("error").innerHTML = "Sorry, we were unable to find any words - please try again later!";
+                document.getElementById('error').innerHTML = 'Sorry, we were unable to find any words - please try again later!';
             }
 
         }
     };
 
-    xhr.open("GET", baseURL + "&length=" + noLetters);
+    xhr.open('GET', baseURL + '&length=' + noLetters);
     xhr.send();
 
 }
@@ -71,8 +71,8 @@ function onlyUnique(value, index, array) {
 function generateWords(noLetters) {
     getData(noLetters, function (data) {
         wordsArray = data.filter(onlyUnique);
-        document.getElementById("words").innerHTML = wordsArray[0];
-        document.getElementById("current-score").innerHTML = 0;
+        document.getElementById('words').innerHTML = wordsArray[0];
+        document.getElementById('current-score').innerHTML = 0;
         document.getElementById('userInput').focus();
     });
     resetTime();
@@ -83,14 +83,14 @@ function generateWords(noLetters) {
 let countPress = 0;
 
 function iteratePress() {
-    let letterCount = document.getElementById("words").innerHTML.split("").length;
+    let letterCount = document.getElementById('words').innerHTML.split('').length;
     if (countPress < letterCount - 1) {
         countPress++;
     } else {
         countPress = 0;
-        document.getElementById("userInput").value = "";
+        document.getElementById('userInput').value = '';
         wordsArray.shift();
-        document.getElementById("words").innerHTML = wordsArray[0];
+        document.getElementById('words').innerHTML = wordsArray[0];
         addAnimal();
         scoreUpdate();
     }
@@ -99,20 +99,20 @@ function iteratePress() {
 //checks if each letter is typed correctly and ends game if not, including updating the score and clearing the words array
 
 function checkLetter(event) {
-    let sourceLetter = document.getElementById("words").innerHTML.charAt(countPress);
+    let sourceLetter = document.getElementById('words').innerHTML.charAt(countPress);
     let inputLetter = event.key;
     if (event.keyCode == 32 || event.keyCode == 13) {
         e.preventDefault();
     }
     else {
         if (inputLetter === sourceLetter) {
-            document.getElementById("userInput").style.color = "#49c195";
+            document.getElementById('userInput').style.color = '#49c195';
         }
         else {
-            document.getElementById("userInput").style.color = "#ee1b29";
+            document.getElementById('userInput').style.color = '#ee1b29';
             document.activeElement.blur();
             clearInterval(timer);
-            wordsArray = "";
+            wordsArray = '';
             let gameOverModal = new bootstrap.Modal(document.getElementById('game-over'));
             gameOverModal.toggle();
             returnScore();
@@ -125,11 +125,11 @@ function checkLetter(event) {
 //resets the game
 
 function resetGame() {
-    $("#counter").html("");
+    $('#counter').html('');
     clearInterval(timer);
     countPress = 0;
-    document.getElementById("words").innerHTML = "";
-    document.getElementById("current-score").innerHTML = 0;
+    document.getElementById('words').innerHTML = '';
+    document.getElementById('current-score').innerHTML = 0;
     score = 1;
     shutdownZoo();
     gameOnOff();
@@ -149,44 +149,44 @@ function levelUpdate(level) {
 let score = 1;
 
 function scoreUpdate() {
-    document.getElementById("current-score").innerHTML = score++;
+    document.getElementById('current-score').innerHTML = score++;
 }
 
 //adds score to congratulations or game over modal
 
 
 function returnScore() {
-    let currentScore = document.getElementById("current-score").innerHTML;
-    document.getElementById("current-game-score").innerHTML = currentScore;
-    document.getElementById("losing-game-score").innerHTML = currentScore;
+    let currentScore = document.getElementById('current-score').innerHTML;
+    document.getElementById('current-game-score').innerHTML = currentScore;
+    document.getElementById('losing-game-score').innerHTML = currentScore;
 }
 
 //Checks and updates high score where required, including local storage
 
 function highScore() {
     if (currentLevel == '4') {
-        if (document.getElementById("current-score").innerHTML > document.getElementById('4-letter').innerHTML) {
-            document.getElementById('4-letter').innerHTML = document.getElementById("current-score").innerHTML;
+        if (document.getElementById('current-score').innerHTML > document.getElementById('4-letter').innerHTML) {
+            document.getElementById('4-letter').innerHTML = document.getElementById('current-score').innerHTML;
         }
     }
     else if (currentLevel == '5') {
-        if (document.getElementById("current-score").innerHTML > document.getElementById('5-letter').innerHTML) {
-            document.getElementById('5-letter').innerHTML = document.getElementById("current-score").innerHTML;
+        if (document.getElementById('current-score').innerHTML > document.getElementById('5-letter').innerHTML) {
+            document.getElementById('5-letter').innerHTML = document.getElementById('current-score').innerHTML;
         }
     }
     else if (currentLevel == '6') {
-        if (document.getElementById("current-score").innerHTML > document.getElementById('6-letter').innerHTML) {
-            document.getElementById('6-letter').innerHTML = document.getElementById("current-score").innerHTML;
+        if (document.getElementById('current-score').innerHTML > document.getElementById('6-letter').innerHTML) {
+            document.getElementById('6-letter').innerHTML = document.getElementById('current-score').innerHTML;
         }
     }
     else if (currentLevel == '7') {
-        if (document.getElementById("current-score").innerHTML > document.getElementById('7-letter').innerHTML) {
-            document.getElementById('7-letter').innerHTML = document.getElementById("current-score").innerHTML;
+        if (document.getElementById('current-score').innerHTML > document.getElementById('7-letter').innerHTML) {
+            document.getElementById('7-letter').innerHTML = document.getElementById('current-score').innerHTML;
         }
     }
     else if (currentLevel == 'random') {
-        if (document.getElementById("current-score").innerHTML > document.getElementById('random-letter').innerHTML) {
-            document.getElementById('random-letter').innerHTML = document.getElementById("current-score").innerHTML;
+        if (document.getElementById('current-score').innerHTML > document.getElementById('random-letter').innerHTML) {
+            document.getElementById('random-letter').innerHTML = document.getElementById('current-score').innerHTML;
         }
     }
     localStorage.setItem('4-letter-local', document.getElementById('4-letter').innerHTML);
@@ -202,13 +202,13 @@ let timer;
 let count = 60;
 
 function startTime() {
-    $("#counter").html(count--);
+    $('#counter').html(count--);
     if (count == -1) {
         document.activeElement.blur();
         highScore();
         clearInterval(timer);
         returnScore();
-        wordsArray = "";
+        wordsArray = '';
         let gameCompleteModal = new bootstrap.Modal(document.getElementById('game-complete'));
         gameCompleteModal.toggle();
         document.getElementById('game-won-modal-button').focus();
@@ -216,7 +216,7 @@ function startTime() {
 }
 
 function resetTime() {
-    $("#counter").html();
+    $('#counter').html();
     clearInterval(timer);
     count = 60;
     timer = setInterval(startTime, 1000);
@@ -224,20 +224,20 @@ function resetTime() {
 
 //adds a random animal to the zoo
 
-let Animals = new Array('<i class="fa-solid fa-hippo"></i>', '<i class="fa-solid fa-otter"></i>', '<i class="fa-solid fa-dragon"></i>', '<i class="fa-solid fa-kiwi-bird"></i>', '<i class="fa-solid fa-crow"></i>', '<i class="fa-solid fa-spider"></i>', '<i class="fa-solid fa-fish-fins"></i>', '<i class="fa-solid fa-frog"></i>', '<i class="fa-solid fa-bugs"></i>', '<i class="fa-solid fa-worm"></i>', '<i class="fa-solid fa-locust"></i>');
+let Animals = new Array('<i class='fa-solid fa-hippo'></i>', '<i class='fa-solid fa-otter'></i>', '<i class='fa-solid fa-dragon'></i>', '<i class='fa-solid fa-kiwi-bird'></i>', '<i class='fa-solid fa-crow'></i>', '<i class='fa-solid fa-spider'></i>', '<i class='fa-solid fa-fish-fins'></i>', '<i class='fa-solid fa-frog'></i>', '<i class='fa-solid fa-bugs'></i>', '<i class='fa-solid fa-worm'></i>', '<i class='fa-solid fa-locust'></i>');
 let zoo = [];
 
 function addAnimal() {
     let randomNumber = Math.floor(Math.random() * Animals.length);
     zoo.push(Animals[randomNumber]);
-    document.getElementById('zooAnimals').innerHTML = zoo.join(" ");
+    document.getElementById('zooAnimals').innerHTML = zoo.join(' ');
 }
 
 //clears the zoo when game is over
 
 function shutdownZoo() {
     zoo = [];
-    document.getElementById('zooAnimals').innerHTML = "";
+    document.getElementById('zooAnimals').innerHTML = '';
 }
 
 //sends message added by user to emailjs or returns error
@@ -255,14 +255,14 @@ if (document.getElementById('form') !== null) {
         emailjs.sendForm(serviceID, templateID, this)
             .then(() => {
                 emailbtn.value = 'Send Email';
-                document.getElementById("email-sent-ack").focus();
-                document.getElementById("form").reset();
+                document.getElementById('email-sent-ack').focus();
+                document.getElementById('form').reset();
             }, (err) => {
                 emailbtn.value = 'Send Email';
                 alert(JSON.stringify(err));
             });
         var modal = document.getElementById('email-sent');
-        modal.style.display = "block";
+        modal.style.display = 'block';
     });
 }
 
